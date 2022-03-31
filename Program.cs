2e40eb63c972ed0116e9ebfd7687e0fa23cb8bf3
annagaetakirsten <anna.agkirsten@gmail.com>
2022-03-31 18:13:48 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int n1, n2, soma;
+
+Console.WriteLine("digite um numero:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("digite outro numero:");
+n2 = Convert.ToInt32(Console.ReadLine())!;
+
+soma = n1 + n2;
+ Console.WriteLine($"Soma: {soma}");
